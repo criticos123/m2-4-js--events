072cf8c8bcea188684db830db8466791d7dbbf5e
Stephanos Criticos <scritico@unb.ca>
2020-06-18 11:48:19 -0400

    exercise 1.0

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,10 @@
 // Exercise 1.0
 // ------------
 console.log('exercise-1');
+const screen= document.querySelector('#bod');
+const text= document.createElement('p');
+bod.addEventListener('click',function(){
+    text.innerText="You Win!";
+    bod.appendChild(text);
+});
+
